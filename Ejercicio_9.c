d1e7814b4acba8a448ce30e7e3702c4e34118a39
JordiAlvarado <86363800+JordiAlvarado@users.noreply.github.com>
2021-07-07 15:51:06 -0300

    Update and rename Ejercicio 9.c to Ejercicio_9.c

--- Ejercicio_9.c
+++ Ejercicio_9.c
@@ -5,19 +5,20 @@ void main ()
 
 {
     int Categoria=0, HorasTrabajadas=0, Sueldo=0;
-    printf ("Ingrese la categoria del empleado y luego sus horas trabajadas");
+    printf ("Ingrese la categoria del empleado ");
     scanf ("%d",&Categoria);
+    printf ("Ingrese las horas trabajadas por el empleado ");
     scanf ("%d",&HorasTrabajadas);
 
     system ("cls");
 
-    if (Categoria=1)
+    if (Categoria==1)
         {
         Sueldo=HorasTrabajadas*200;
         }
     else
         {
-        if (Categoria=2)
+        if (Categoria==2)
             {
             Sueldo=HorasTrabajadas*180;
             }
