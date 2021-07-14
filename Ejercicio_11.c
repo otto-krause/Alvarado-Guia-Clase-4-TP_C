8ad3ac38666f6d86c27d3a98a30fe41d79f65d75
JordiAlvarado <86363800+JordiAlvarado@users.noreply.github.com>
2021-07-14 20:53:36 -0300

    Update Ejercicio_11.c

--- Ejercicio_11.c
+++ Ejercicio_11.c
@@ -4,27 +4,28 @@
 void main ()
 
 {
-    int P=0, A=0, IMC=0;
+    int P=0, IMC=0;
+    float A=0;
     printf("Ingrese su peso ");
     scanf("%d",&P);
     printf ("Ingrese su altura ");
-    scanf("%d",&A);
+    scanf("%f",&A);
 
     system ("cls");
 
-    IMC=P/(A*A);
+    IMC=P/(A*2);
 
-    if (IMC>25)
+    if (IMC>=25)
         {
         printf("La persona se encuentra excedida de peso");
         }
-    else 
+    else
         {
         if(IMC>=20)
             {
             printf("La persona se encuentra en su pedo ideal");
             }
-        else 
+        else
             {
             printf("La persona se encuentra por debajo de su peso ideal");
             }
