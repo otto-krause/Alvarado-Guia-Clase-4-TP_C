c2275c45f605cf9417f53fa3fcdb3d3ab47a840b
JordiAlvarado <86363800+JordiAlvarado@users.noreply.github.com>
2021-07-14 20:42:26 -0300

    Update Ejercicio_12.c

--- Ejercicio_12.c
+++ Ejercicio_12.c
@@ -5,14 +5,14 @@ void main ()
 
 {
     int SP1=0,SP2=0,SP3=0,PP1=0,PP2=0,PP3=0,DP1=0,DP2=0,DP3=0;
-    printf("Ingrese la superficie de cada pais ");
-    scanf("%d",SP1);
-    scanf("%d",SP2);
-    scanf("%d",SP3);
-    printf ("Ingrese la poblacion de cada pais ");
-    scanf("%d",PP1);
-    scanf("%d",PP2);
-    scanf("%d",PP3);
+    printf("Ingrese la superficie de los paises ");
+    scanf("%d",&SP1);
+    scanf("%d",&SP2);
+    scanf("%d",&SP3);
+    printf("Ingrese la poblacion de los paises ");
+    scanf("%d",&PP1);
+    scanf("%d",&PP2);
+    scanf("%d",&PP3);
 
     system ("cls");
 
@@ -29,7 +29,7 @@ void main ()
             {
             printf("El pais mas densamente poblado es el segundo");
             }
-        else 
+        else
             {
             printf("El pais mas densamente poblado es el tercero");
             }
